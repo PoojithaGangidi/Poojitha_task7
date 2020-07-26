fd129cf1e8e85ce0b10740552e78f442e97b9210
PoojithaGangidi <poojithareddy.9051@gmail.com>
2020-07-26 19:56:31 +0530

    commit

---
+++ Task7/src/FactoryEx/Rectangle.java
@@ -0,0 +1,8 @@
+package FactoryEx;
+
+public class Rectangle  implements Shape {
+
+	   public void draw() {
+	      System.out.println("Inside Rectangle::draw() method.");
+	   }
+}
---
+++ Task7/src/FactoryEx/Shape.java
@@ -0,0 +1,5 @@
+package FactoryEx;
+
+public interface Shape {
+	void draw();
+}
